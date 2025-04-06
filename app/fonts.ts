63f164f7c7da5b8277a49c9727e4967f894569c1
nerdboi008 <mail.nerdboi@gmail.com>
2025-04-06 23:36:41 +0530

    Refactor styles and layout for Nerdboi portfolio; add footer component and update dependencies

---
+++ app/fonts.ts
@@ -0,0 +1,13 @@
+import { Geist, Homenaje, } from "next/font/google";
+
+export const fontPrimary = Geist({
+    variable: '--font-geist',
+    display: 'swap',
+    weight: "400"
+})
+
+export const fontSecondary = Homenaje({
+    variable: '--font-homenaje',
+    display: 'swap',
+    weight: "400"
+})
---
+++ components/common/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+      <footer className='text-muted-foreground flex items-center justify-center py-3'>
+          <p>visit github</p>
+      </footer>
+  )
+}
+
+export default Footer
